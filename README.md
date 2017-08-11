9cae1ad38ed1e00ac40dec7560833e9d1d680cae
System <none>
2017-08-11 03:41:03 +0000

    Checkpoint

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 This project provides an example for fetching data from Airtable
 using [airtable.js](https://github.com/Airtable/airtable.js)
 
-It uses the ["Product Catalog & Orders" template](https://airtable.com/templates/featured/expZvMLT9L6c4yeBX/product-catalog-and-orders).
+To make your own:
 
-Put your Airtable API key and base ID in .env to get started!
-
-[airtable.com/api](https://airtable.com/api)
+1. Log into for Airtable and copy the ["Product Catalog & Orders" template](https://airtable.com/templates/featured/expZvMLT9L6c4yeBX/product-catalog-and-orders).
+2. Go to [airtable.com/account](https://airtable.com/account) to generate an API key
+3. Put your Airtable API key and base ID in .env to get started! You can fidn the base ID on [airtable.com/api](https://airtable.com/api)
--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 // server.js
 // where your node app starts
 
-// init project
-var express = require('express');
-var app = express();
-
 // We're going to use the "Product Catalog and Orders" base template:
 // https://airtable.com/templates/featured/expZvMLT9L6c4yeBX/product-catalog-and-orders
 var Airtable = require('airtable');
@@ -12,8 +8,10 @@ var base = new Airtable({
   apiKey: process.env.AIRTABLE_API_KEY,
 }).base(process.env.AIRTABLE_BASE_ID);
 var tableName = 'Furniture';
+var viewName = 'Main View';
 
-var request = require('request');
+var express = require('express');
+var app = express();
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -33,10 +31,10 @@ app.get("/data", function (_, response) {
   if (cachedResponse && new Date() - cachedResponseDate < cacheTimeoutMs) {
     response.send(cachedResponse);
   } else {
-    // Select the first 10 records in "Main View".
+    // Select the first 10 records from the view.
     base(tableName).select({
       maxRecords: 10,
-      view: 'Main View',
+      view: viewName,
     }).firstPage(function(error, records) {
       if (error) {
         response.send({error: error});
