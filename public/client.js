dbfb258e15bfdc54b78fff8643f9cb1c1b66b0d1
Glitch (airtable-example) <none>
2020-06-13 23:54:54 +0000

    🚍🐟 Checkpoint ./public/client.js:16584221/7860 ./server.js:16584221/7165 ./views/cutit.html:16584221/92

--- public/client.js
+++ public/client.js
@@ -1,31 +1,58 @@
 // client-side js
 // run by the browser each time your view template is loaded
 
-// TODO(vicki): only fetch the data that we need per page
-$(function() {
-  $.getJSON('/cutit=data', )
+var usdFormatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
 })
 
+// TODO(vicki): only fetch the data that we need per page
 $(function() {
-  $.getJSON('/data', function(data) {
-    var $dataContainer = $('#data-container');
-      
+  $.getJSON('/cutit-data', function(data) {
+    var $container = $('#cutit-container');
+    
     if (data.error) {
-      $dataContainer.html('Error! ' + data.error);
+      // TODO(vicki): figure out how to show the error?
+      $container.html('Hmm, an error occurred.');
       return;
     }
-      
+    
     // Clear the loading message.
-    $dataContainer.html('');
+    $container.html('');
     
     data.records.forEach(function(record) {
-      var $galleryCard = $('<div class="gallery-card" />');
-      if (record.picture[0]) {
-        $('<img />').attr('src', record.picture[0].url).appendTo($galleryCard);
-      }
+      var $card = $('<div class="record-card" />');
       var $label = $('<strong />').text(record.name);
-      $galleryCard.append($label);
-      $dataContainer.append($galleryCard);
+      $card.append($label);
+      var $amount = $('<p class="record-amount" />').text(usdFormatter.format(record.amount));
+      $card.append($amount);
+      var $descTitle = $('<p class="record-title" />').text("")
+      $container.append($card);
     });
+    console.log('Data: ', data);
   });
 });
+
+// $(function() {
+//   $.getJSON('/data', function(data) {
+//     var $dataContainer = $('#data-container');
+      
+//     if (data.error) {
+//       $dataContainer.html('Error! ' + data.error);
+//       return;
+//     }
+      
+//     // Clear the loading message.
+//     $dataContainer.html('');
+    
+//     data.records.forEach(function(record) {
+//       var $galleryCard = $('<div class="gallery-card" />');
+//       if (record.picture[0]) {
+//         $('<img />').attr('src', record.picture[0].url).appendTo($galleryCard);
+//       }
+//       var $label = $('<strong />').text(record.name);
+//       $galleryCard.append($label);
+//       $dataContainer.append($galleryCard);
+//     });
+//   });
+// });
--- server.js
+++ server.js
@@ -3,36 +3,36 @@
 
 // We're going to use the "Product Catalog and Orders" base template:
 // https://airtable.com/templates/featured/expZvMLT9L6c4yeBX/product-catalog-and-orders
-var Airtable = require('airtable');
+var Airtable = require("airtable");
 var base = new Airtable({
-  apiKey: process.env.AIRTABLE_API_KEY,
+  apiKey: process.env.AIRTABLE_API_KEY
 }).base(process.env.AIRTABLE_BASE_ID);
 
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + "/views/index.html");
 });
 
 app.get("/cutit", function(request, response) {
-  response.sendFile(__dirname + '/views/cutit.html');
+  response.sendFile(__dirname + "/views/cutit.html");
 });
 
 app.get("/reinvest", function(request, response) {
-  response.sendFile(__dirname + '/views/reinvest.html');
+  response.sendFile(__dirname + "/views/reinvest.html");
 });
 
 app.get("/swap", function(request, response) {
-  response.sendFile(__dirname + '/views/swap.html');
+  response.sendFile(__dirname + "/views/swap.html");
 });
 
 app.get("/gallery", function(request, response) {
-  response.sendFile(__dirname + '/views/gallery.html');
+  response.sendFile(__dirname + "/views/gallery.html");
 });
 
 // Cache the records in case we get a lot of traffic.
@@ -42,61 +42,73 @@ var cachedResponse = null;
 var cachedResponseDate = null;
 
 app.get("/cutit-data", function(_, response) {
-  var tableName = 'Cut It (NYPD Budget)';
-  var viewName = 'Grid view';
+  var tableName = "Cut It (NYPD Budget)";
+  var viewName = "Grid view";
   if (cachedResponse && new Date() - cachedResponseDate < cacheTimeoutMs) {
     response.send(cachedResponse);
   } else {
     // Select the first 10 records from the view.
-    // TODO(vicki): does this paginate? 
-    base(tableName).select({
-      maxRecords: 10,
-      view: viewName,
-    }).firstPage(function(error, records) {
-      if (error) {
-        response.send({error: error});
-      } else {
-        cachedResponse = {
-          records: records,
-        };
-        cachedResponseDate = new Date();
-        response.send(cachedResponse);
-      }
-    });
+    // TODO(vicki): does this paginate?
+    base(tableName)
+      .select({
+        maxRecords: 10,
+        view: viewName
+      })
+      .firstPage(function(error, records) {
+        if (error) {
+          response.send({ error: error });
+        } else {
+          cachedResponse = {
+            records: records.map(record => {
+              return {
+                name: record.get("What to cut?"),
+                description: record.get("What is it?"),
+                amount: record.get("How much is it?"),
+                rationale: record.get("Why should we cut it?"),
+                data: record.get("Any data or sources to share?")
+              };
+            }),
+          };
+          cachedResponseDate = new Date();
+          response.send(cachedResponse);
+        }
+      });
   }
 });
 
 app.get("/data", function(_, response) {
-  var tableName = 'Furniture';
-  var viewName = 'Furniture gallery';
+  var tableName = "Furniture";
+  var viewName = "Furniture gallery";
   if (cachedResponse && new Date() - cachedResponseDate < cacheTimeoutMs) {
     response.send(cachedResponse);
   } else {
     // Select the first 10 records from the view.
-    base(tableName).select({
-      maxRecords: 10,
-      view: viewName,
-    }).firstPage(function(error, records) {
-      if (error) {
-        response.send({error: error});
-      } else {
-        cachedResponse = {
-          records: records.map(record => {
-            return {
-              name: record.get('Name'),
-              picture: record.get('Images'),
-            };
-          }),
-        };
-        cachedResponseDate = new Date();
-        
-        response.send(cachedResponse);
-      }
-    });
+    base(tableName)
+      .select({
+        maxRecords: 10,
+        view: viewName
+      })
+      .firstPage(function(error, records) {
+        if (error) {
+          response.send({ error: error });
+        } else {
+          cachedResponse = {
+            records: records.map(record => {
+              return {
+                name: record.get("Name"),
+                picture: record.get("Images")
+              };
+            })
+          };
+          cachedResponseDate = new Date();
+
+          response.send(cachedResponse);
+        }
+      });
   }
 });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
